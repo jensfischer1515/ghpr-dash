eea7c81748a43ce61c14dc8189700e6680ca8f65
Jens Fischer <jens.fischer@otto.de>
2020-02-04 20:56:11 +0100

    add more fontawesome icons

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(fas)
+library.add(fas, far, fab); // see https://github.com/FortAwesome/vue-fontawesome#import-entire-styles
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
