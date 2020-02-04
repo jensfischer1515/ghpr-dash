59d2b873df956879bfdb3d5308aac82e16640b30
Jens Fischer <jens.fischer@otto.de>
2020-02-04 22:25:20 +0100

    filter by PR state

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,11 +9,85 @@
 import gql from "graphql-tag";
 
 const pullRequestsQuery = gql`
-  query PullRequests($organization: String!, $team: String!) {
-    organization(login: $organization) {
+  query PullRequests(
+      $organizationLogin: String!,
+      $teamSlug: String!,
+      $numberOfRepositories: Int = 100,
+      $numberOfPullRequests: Int = 25,
+      $numberOfAssignees: Int = 10,
+      $numberOfLabels: Int = 10
+    ) {
+    organization(login: $organizationLogin) {
       name
-      team(slug: $team) {
+      team(slug: $teamSlug) {
         name
+        repositories(first: $numberOfRepositories, orderBy: {field: NAME, direction: ASC}) {
+          nodes {
+            name
+            url
+            primaryLanguage {
+              name
+            }
+            isFork
+            isPrivate
+            isArchived
+            isDisabled
+            isLocked
+            isTemplate
+            isMirror
+            mergeCommitAllowed
+            rebaseMergeAllowed
+            squashMergeAllowed
+            deleteBranchOnMerge
+            pullRequests(states: OPEN, first: $numberOfPullRequests, orderBy: {field: UPDATED_AT, direction: DESC}) {
+              nodes {
+                url
+                number
+                title
+                #bodyHTML
+                state
+                closed
+                merged
+                locked
+                lastEditedAt
+                publishedAt
+                updatedAt
+                mergeable
+                baseRef {
+                  name
+                  repository {
+                    url
+                  }
+                }
+                headRef {
+                  name
+                  repository {
+                    url
+                  }
+                }
+                assignees(first: $numberOfAssignees) {
+                  nodes {
+                    login
+                  }
+                }
+                author {
+                  login
+                }
+                labels(first: $numberOfLabels) {
+                  nodes {
+                    name
+                  }
+                }
+                changedFiles
+                additions
+                deletions
+                commits {
+                  totalCount
+                }
+              }
+            }
+          }
+        }
       }
     }
   }
@@ -24,8 +98,9 @@ export default {
 
   data: function() {
     return {
-      organization: {},
-      skipQuery: false,
+      organization: null,
+      team: null,
+      repositories: [],
     };
   },
 
@@ -35,7 +110,6 @@ export default {
 
   methods: {
     fetchPullRequests: async function() {
-      this.$apollo.queries.pullRequests.skip = false;
       await this.$apollo.queries.pullRequests.refetch();
     },  
   },
@@ -47,23 +121,22 @@ export default {
       },
       variables() {
         return {
-          organization: process.env.VUE_APP_GITHUB_ORGANIZATION,
-          team: process.env.VUE_APP_GITHUB_TEAM
+          organizationLogin: process.env.VUE_APP_GITHUB_ORGANIZATION,
+          teamSlug: process.env.VUE_APP_GITHUB_TEAM
         };
       },
       skip() {
-        return this.skipQuery;
+        return false;
       },
       update(data) {
-        console.info(
-          `Skip GraphQL query? @ ${this.$options.name}`,
-          this.skipQuery
-        );
-        if (this.skipQuery) {
-          return;
-        }
-        this.organization = data.organization;
-        this.skipQuery = true;
+        this.organization = data.organization.name;
+        this.team = data.organization.team.name;
+        this.repositories = data.organization.team.repositories.nodes.filter(function (el) {
+          let hasOpenPullRequests = el.pullRequests.nodes.some(function (el){
+            return el.closed == false;
+          })
+          return hasOpenPullRequests;
+        });
       }
     }
   },
